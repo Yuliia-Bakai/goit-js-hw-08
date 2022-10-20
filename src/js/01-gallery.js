4a63eb4ada33f08d854205a0e74c3c271b47d43b
Yuliia-Bakai <lera.smolina1@gmail.com>
2022-10-20 16:45:24 +0300

    +

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const markup = galleryItems.map(({ original, preview, description }) =>
+    `<a class="gallery__item" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        />
+    </a>`
+).join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+})
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,61 @@
+import throttle from 'lodash.throttle';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+Notify.init({
+  width: '400px',
+  position: 'center-top',
+  fontSize: '18px',
+});
+
+const form = document.querySelector('.feedback-form');
+// const email = document.querySelector('.feedback-form input');
+// const message = document.querySelector('.feedback-form textarea');
+
+const formDataObj = {};
+const FORM_DATA = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+dataRecovery();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  let isEmptyField = false;
+  let messageFields = '';
+  const formData = new FormData(form);
+  formData.forEach((value, name) => {
+    if (value === '') {
+      messageFields += form.elements[name].parentNode.textContent.trim() + ', ';
+      isEmptyField = true;
+      formDataObj[name] = value;
+    }
+  });
+  if (isEmptyField) {
+    Notify.failure(`Fields ${messageFields} must be filled!`);
+    // alert(`Fields ${messageFields} must be filled!`);
+    return;
+  }
+
+  console.log(formDataObj);
+  e.currentTarget.reset();
+  localStorage.removeItem(FORM_DATA);
+}
+
+function onFormInput(e) {
+  const formData = new FormData(form);
+  formData.forEach((value, name) => {
+    formDataObj[name] = value;
+  });
+
+  localStorage.setItem(FORM_DATA, JSON.stringify(formDataObj));
+}
+
+function dataRecovery() {
+  const data = JSON.parse(localStorage.getItem(FORM_DATA));
+  if (data) {
+    Object.entries(data).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
+  }
+}
